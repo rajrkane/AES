495e2df9a7b4699f1ccc627d64f20e5fe7b47578
JacobR098 <44142718+JacobR098@users.noreply.github.com>
2021-03-11 12:05:39 -0700

    Add encrypt header file

--- src/encrypt.hpp
+++ src/encrypt.hpp
@@ -2,4 +2,15 @@
 #ifndef ENCRYPT_HPP
 #define ENCRYPT_HPP 
 
+void keyExpansion(unsigned char* key);
+void subBytes(unsigned char* state);
+void shiftRows(unsigned char* state);
+void mixColumns(unsigned char* state);
+void addRoundKey(unsigned char* state);
+
+
+
+void encrypt(unsigned char* input, unsigned char* output);
+
+
 #endif
