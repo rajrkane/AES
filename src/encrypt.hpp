2496873e6518f239309ffd177503d5b9631a27d0
Eric Bell <ericbell495@gmail.com>
2021-03-18 00:21:12 -0400

    Fix and verify correctness
    Fixed syntax errors and other compilation errors and brought this branch up to date with the other code.
    Checked correctness based on the AES specification, Appendix B.

--- src/encrypt.hpp
+++ src/encrypt.hpp
@@ -2,4 +2,16 @@
 #ifndef ENCRYPT_HPP
 #define ENCRYPT_HPP 
 
+void encrypt(unsigned char* plainInput, unsigned char* encryptedOutput, unsigned char* key, unsigned int keysize);
+
+void keyExpansion(unsigned char* key, unsigned char* expansion, unsigned char keysize);
+
+void addRoundKey(unsigned char* state, unsigned char* key);
+
+void subBytes(unsigned char* state);
+
+void shiftRows(unsigned char* state);
+
+void mixColumns(unsigned char* state);
+
 #endif
