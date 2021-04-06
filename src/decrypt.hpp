bcc3f5e93e8c42e9a3a5b02e7a1c61ade24b51ea
rrkane <raj.r.kane@protonmail.com>
2021-04-05 21:54:14 -0700

    moved #include "AESmath.hpp" to decrypt.hpp

--- src/decrypt.hpp
+++ src/decrypt.hpp
@@ -2,12 +2,11 @@
 #ifndef DECRYPT_HPP
 #define DECRYPT_HPP
 
+#include "AESmath.hpp"
+
 void decrypt(unsigned char* input, unsigned char* output, unsigned char* key, int keysize);
-
 void invSubBytes(unsigned char* state);
-
 void invShiftRows(unsigned char* state);
-
 void invMixColumns(unsigned char* state);
 
 #endif
